bc34874d8389ed0c58b81267e37ab72219bd0b63
Ch3shireDev <igor.nowicki.business@gmail.com>
2021-08-09 22:47:06 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'smbc-browser-3',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
